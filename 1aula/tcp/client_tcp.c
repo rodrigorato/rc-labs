d238349c9f667b1c25526557f0ecc2436f52afd5
Rodrigo Rato RC Labs <rodrigorato@tecnico.ulisboa.pt>
2016-09-20 16:31:36 +0100

    all done

--- 1aula/tcp/client_tcp.c
+++ 1aula/tcp/client_tcp.c
@@ -8,36 +8,36 @@
 #include <string.h>
 
 #define PORT 59000
-#define MESSAGE "Segmentation fault"
+#define MESSAGE "hello"
 
 int main(){	
-	int fd, addrlen;
+	int fd, clientlen, newfd;
 	struct hostent* hostptr;
 	struct sockaddr_in serveraddr, clientaddr;
 	char buffer[128];	
 	
-	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) exit(1);
-
-	hostptr = gethostbyname("minho.tecnico.ulisboa.pt");
+	if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) exit(1);
 	
+	hostptr = gethostbyname("lima.tecnico.ulisboa.pt");
+		
 	memset((void*)&serveraddr, (int)'\0', sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_addr.s_addr = ((struct in_addr*) (hostptr->h_addr_list[0]))->s_addr;
 	serveraddr.sin_port = htons((u_short)PORT);
 	
-	addrlen = sizeof(serveraddr);
+	
+	if(connect(fd, (struct sockaddr*) &serveraddr, sizeof(serveraddr)) == -1) exit(1);
+	printf("Connected successfully\n");
 
-	if(sendto(fd, MESSAGE, strlen(MESSAGE) + 1, 0, (struct sockaddr*) &serveraddr, addrlen) == -1) exit(1);
-	
-	printf("Sent message:\n%s\n", MESSAGE);
-	
-	if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*) &serveraddr, &addrlen) == -1) exit(1);
-	
+
+	if(write(fd, MESSAGE, strlen(MESSAGE)) == -1) exit(1);
+	printf("Sent message:\n%s\n", MESSAGE);	
+
+
+	if(read(fd, buffer, 128) == -1) exit(1);
 	printf("Received message:\n%s\n", buffer); 
 
 	
-		
-
 	close(fd);
 
 	return 0;
