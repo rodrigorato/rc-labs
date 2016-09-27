88b2ce4309070018ae7bd7d80e659d6ea9084445
Alice Dourado <alice.dourado1@gmail.com>
2016-09-27 16:22:34 +0100

    ja fala com o tejo

--- projeto/user_app/user.cpp
+++ projeto/user_app/user.cpp
@@ -70,16 +70,16 @@ int main(int argc, char** argv){
 
 			if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) exit(1);//udp
 
-			hostptr = gethostbyname(TCSNAME);
+			hostptr = gethostbyname(TCSname.c_str());
 			
 			memset((void*)&serveraddr, (int)'\0', sizeof(serveraddr));
 			serveraddr.sin_family = AF_INET;
 			serveraddr.sin_addr.s_addr = ((struct in_addr*) (hostptr->h_addr_list[0]))->s_addr;
-			serveraddr.sin_port = htons((u_short)TCSPORT);
+			serveraddr.sin_port = htons((u_short)TCSport);
 			
 			addrlen = sizeof(serveraddr);
 
-			if(sendto(fd, MESSAGE1, strlen(MESSAGE1) + 1, 0, (struct sockaddr*) &serveraddr, addrlen) == -1) exit(1);
+			if(sendto(fd, MESSAGE1, strlen(MESSAGE1), 0, (struct sockaddr*) &serveraddr, addrlen) == -1) exit(1);
 			
 			printf("Sent message:%s", MESSAGE1);//ULQ
 		}
