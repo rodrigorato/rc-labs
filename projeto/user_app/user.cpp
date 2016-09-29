c6bd0af7abceb0017dcbe215f81219508b9a8384
Alice Dourado <alice.dourado1@gmail.com>
2016-09-29 23:39:16 +0100

    parece funcionar tudo menos ficheiros

--- projeto/user_app/user.cpp
+++ projeto/user_app/user.cpp
@@ -26,13 +26,42 @@
 #define MAX_FILE_NAME  99 //idk
 
 using namespace std;
-//por if em gethosts, perguntar ao user what do do se o tejo nao responde 
+//por if em gethosts, perguntar ao user what do do se o tejo nao responde , verificar se os argumentos tao certos,ostringstream
+//wantsAnswer é solucao ate se separar melhor as coisas
+
+bool isError(char buff[]){
+	if(buff[4]=='E'){ // TCS devolve mensagem de erro
+		char mRead[3];
+		char m[3];
+		sscanf(buff,"%s %s",mRead, m);
+		if(!strcmp(m,"EOF")) printf("pedido nao pode ser cumprido\n");
+		else if (!strcmp(m,"ERR")) printf("Wrong instruction\n");
+	else if (!strcmp(m,"NTA")) printf("translation is not available\n");
+	else return false;
+	return true;
+	}
+}
+/*
+int startUDP(int fd,struct hostent* hostptr,string TCSname,struct sockaddr_in serveraddr,int TCSport){
+	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) exit(1);//udp
+
+		hostptr = gethostbyname(TCSname.c_str());
+			
+		memset((void*)&serveraddr, (int)'\0', sizeof(serveraddr));
+		serveraddr.sin_family = AF_INET;
+		serveraddr.sin_addr.s_addr = ((struct in_addr*) (hostptr->h_addr_list[0]))->s_addr;
+		serveraddr.sin_port = htons((u_short)TCSport);
+			
+		addrlen = sizeof(serveraddr);
+}*/
+
+
 int main(int argc, char** argv){	
 	int fd;
 	socklen_t addrlen;
 	struct hostent* hostptr;
 	struct sockaddr_in serveraddr, clientaddr;
-	char buffer[128], instruction[8];
+	char buffer[128];//,instruction[8];
 	char message[128];   //  buffer and message have a random bullshit size
 	int TCSport=TCSPORT;
 	string TCSname=TCSNAME;
@@ -40,9 +69,10 @@ int main(int argc, char** argv){
 	char languages[MAX_LANGS][MAX_SIZE_LANGUAGE], words[MAX_WORDS][MAX_SIZE_WORD];
 	char tf;//indicador se vai-se traduzir texto ou ficheiro
 	int numWords;
+	bool wantsAnswer=false;
 	//./user -n TCSname -p TCSport
 
-	if(argc < 1 || argc > 5)
+	if(argc < 1 || argc > 5)  // reading input
 		printf("wrong input\n");
 	for(int i = 1; i < argc; i+=2){
 		if(argv[i][0] != '-')
@@ -66,10 +96,21 @@ int main(int argc, char** argv){
 				printf("wrong input\n");
 		}
 	}
-	
-	scanf("%s", instruction);
-	while(strcmp(instruction,"exit")){ 
-		if (!strcmp(instruction,"list")){       
+	string user_input,instruction;
+
+	stringstream input_stream;
+	getline(cin, user_input);
+	input_stream << user_input;
+	input_stream >> instruction; 
+
+	/*for( i=0;user_input[i]!=' ';i++){
+		instruction[i]=user_input[i];
+	}*/
+	//scanf("%s", instruction);
+
+
+	while(strcmp(instruction.c_str(),"exit")){                      // ciclo de espera por input do utilizador
+		if (!strcmp(instruction.c_str(),"list")){       
 
 			if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) exit(1);//udp
 
@@ -85,109 +126,144 @@ int main(int argc, char** argv){
 			if(sendto(fd, MESSAGE1, strlen(MESSAGE1), 0, (struct sockaddr*) &serveraddr, addrlen) == -1) exit(1);
 			
 			printf("Sent message:%s", MESSAGE1);//ULQ
+			wantsAnswer=true;
 		}
-		else if (!strcmp(instruction,"request")){
+		else if (!strcmp(instruction.c_str(),"request")){ //request n t palvra
 			int langNum;
-			scanf("%d %c", &langNum, &tf );
+
+			input_stream >> langNum;
+			input_stream >> tf;
+
 
 			//UNQ + language   languages[langNum-1]
 			string message2="UNQ ";
-			string temp= languages[langNum-1];
-			message2+=temp;
+			string temp2= languages[langNum-1];
+			message2+=temp2;
 			message2+='\n';
-
+			
 			if(sendto(fd, message2.c_str(), message2.length() + 1, 0, (struct sockaddr*) &serveraddr, addrlen) == -1) exit(1);
 			printf("Sent message: %s", message2.c_str());//UNQ language
+
 			if(tf=='t'){
-				scanf("%d", &numWords);
 				printf("Palavras a traduzir para %s:\n",languages[langNum-1]);
-				for(int i=0; i<numWords;i++){
-					scanf("%s",words[i]);
+				char temp3[MAX_SIZE_WORD];
+				int i;
+				for(i=0;input_stream >> words[i];i++){
 					printf("%s\n",words[i]);
 				}
+				numWords=i;
+
+				
 			}else if(tf=='f'){
 				char filename[MAX_FILE_NAME];  //provavelmente sitiu mau pa declarar isto????????
 				scanf("%s",filename);
 			}
+			wantsAnswer=true;
 
 		}
-		int num_bytes=0;
-		if((num_bytes=recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*) &serveraddr, &addrlen)) == -1) exit(1);
-		buffer[num_bytes]='\0';
-		printf("Received message:\n%s\n",buffer); 
+		if (wantsAnswer){
+			int num_bytes=0;                                   // recieving message from central server
+			if((num_bytes=recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*) &serveraddr, &addrlen)) == -1) exit(1);
+			buffer[num_bytes]='\0';
+			printf("Received message:\n%s",buffer); 
+	//to do: por issto numa funçao á parte
+			sscanf(buffer,"%s", message);
+			if (!isError(buffer)){
+				if(!strcmp(message,"ULR")){    //ULR , resposta ao pedido de linguas
+					int numLangs;
+					sscanf(buffer,"%s %d",message, &numLangs);//ULR 3 frances ingles whatever
+					printf("Languages available:\n");
 
-		sscanf(buffer,"%s", message);
-		if(buffer[4]=='E'){ // TCS devolve mensagem de erro
-			char m[3];
-			sscanf(buffer,"%s %s",message, m);
-			if(!strcmp(m,"EOF")) printf("pedido nao pode ser cumprido\n");
-			else if (!strcmp(m,"ERR")) printf("Wrong instruction\n");
-		}else if(!strcmp(message,"ULR")){    //ULR , resposta ao pedido de linguas
-			int numLangs;
-			sscanf(buffer,"%s %d",message, &numLangs);
-			printf("Languages available:\n");/*
-			for(int i=0; i<numLangs;i++){//                  ULR 3 frances ingles whatever
-
-				sscanf(buffer,"%s %d %s",message,&numLangs,languages[i]);
-				printf("%d - %s\n", i+1, languages[i]);
-			}*/
-			int i, j;
-			if(numLangs>9) i=7;
-			else i=6;
-			
-			for(int l=0; l<numLangs;l++){
-				for (j=0; buffer[i]!=' ' && buffer[i]!='\n' /*&& buffer[i]!='\0'*/;j++,i++)
-					languages[l][j]=buffer[i];
+					int i, j;
+					if(numLangs>9) i=7;
+					else i=6;
+					
+					for(int l=0; l<numLangs;l++){
+						for (j=0; buffer[i]!=' ' && buffer[i]!='\n' /*&& buffer[i]!='\0'*/;j++,i++)
+							languages[l][j]=buffer[i];
+						languages[l][j]='\0'; 
+						i++;
+						printf("%d - %s\n", l+1, languages[l]);
+					}
 				
-				languages[l][j]='\0'; 
-				i++;
-				printf("%d - %s\n", l+1, languages[l]);
+				}else if (!strcmp(message,"UNR")){//UNR lingua ip port, e depois comunicacao com trs
+					char ipTRS[16];    //provavelmente sitiu mau pa declarar isto??????
+					int portTRS;
+					sscanf(buffer, "%s %s %d",message, ipTRS,&portTRS); //id é string num ou struct hostent*?
+
+					//inicio comunicacao com TRS usando TCP
+					int fd2;
+					struct hostent* hostptr2;
+					struct sockaddr_in serveraddr2;
+					char buffer2[128];
+
+					if((fd2 = socket(AF_INET, SOCK_STREAM, 0)) == -1) exit(1);
+					if((hostptr2=gethostbyname(ipTRS))==NULL) printf("nope\n"); // to do: exit(1)
+
+					memset((void*)&serveraddr2, (int)'\0', sizeof(serveraddr2));
+					serveraddr2.sin_family = AF_INET;
+					serveraddr2.sin_addr.s_addr = ((struct in_addr*) (hostptr2->h_addr_list[0]))->s_addr;//seg fault
+					serveraddr2.sin_port = htons((u_short)portTRS);
+
+					if(connect(fd2, (struct sockaddr*) &serveraddr2, sizeof(serveraddr2)) == -1) exit(1);//ta testado ate aqui pk nao trs
+					printf("Connected successfully\n");
+
+					if(tf=='t'){
+						stringstream stream;
+						string message3 = "TRQ t ";
+						stream << numWords;
+						message3 += stream.str();
+						
+						for(int i=0; i<numWords;i++){	
+							stringstream s ;
+							s<< words[i];
+							message3+= ' ' + s.str();
+							cout<< message3<<endl;
+						}
+						message3 += '\n';
+						if(write(fd2, message3.c_str(), message3.length()) == -1) exit(1);
+						cout << "sent message: \n" << message3 << endl;			
+					}
+
+					if(read(fd2, buffer2, 128) == -1) exit(1);
+					buffer2[num_bytes]='\0';
+					printf("Received message:\n%s\n", buffer2); 
+					if(!isError(buffer2)){
+						
+						stringstream st;
+						st.str(buffer2);
+						st >> buffer2;//lixo, TRR é preciso verificar?
+						st >> tf;
+						st >>numWords;
+						if (tf=='t'){
+							cout << "traducao:"<<endl;
+							for(int i=0;st >> words[i];i++)
+								cout<<words[i]<<endl;
+						}
+					}
+					printf("abbbbbaaa\n");
+
+
+					close(fd2);
+				}
 			}
-			
-		}else if (!strcmp(message,"UNR")){
-			char ipTRS[16];    //provavelmente sitiu mau pa declarar isto??????
-			int portTRS;
-			sscanf(buffer, "%s %s %d",message, ipTRS,&portTRS); //id é string num ou struct hostent*?
-
-			//inicio comunicacao com TRS usando TCP
-			int fd2;
-			struct hostent* hostptr2;
-			struct sockaddr_in serveraddr2;
-			char buffer2[128];
-
-			if((fd2 = socket(AF_INET, SOCK_STREAM, 0)) == -1) exit(1);
-			if((hostptr2=gethostbyname(ipTRS))==NULL) printf("nope\n");
-
-			memset((void*)&serveraddr2, (int)'\0', sizeof(serveraddr2));
-			serveraddr2.sin_family = AF_INET;
-			serveraddr2.sin_addr.s_addr = ((struct in_addr*) (hostptr2->h_addr_list[0]))->s_addr;//seg fault
-			serveraddr2.sin_port = htons((u_short)portTRS);
-
-			if(connect(fd2, (struct sockaddr*) &serveraddr2, sizeof(serveraddr2)) == -1) exit(1);//ta testado ate aqui pk nao trs
-			printf("Connected successfully\n");
-
-			if(tf=='t'){
-				ostringstream stream;
-				string message3 = "TRQ t ";
-				stream << numWords;
-				message3 += stream.str();
-				for(int i=0; i<numWords;i++)	
-					message3 += ' ' + words[i];
-				message3 += '\n';
-				if(write(fd2, message3.c_str(), message3.length()) == -1) exit(1);
-				cout << "sent message: \n" << message3 << endl;	
-				
-			}
-
-
-			if(read(fd2, buffer2, 128) == -1) exit(1);
-			printf("Received message:\n%s\n", buffer2); 
-			
-			close(fd2);
-
 		}
-
-		scanf("%s", instruction);
+		wantsAnswer=false;
+		getline(cin, user_input);
+		//input_stream.str(input_stream.str().replace(0,input_stream.str().length(),user_input));
+		//input_stream << user_input;
+		stringstream s;
+		s.str(input_stream.str());
+		s<<user_input;
+		s.swap(input_stream);
+		input_stream >> instruction;
+		cout<<instruction<<endl;
+		
+		/*
+		for( i=0;user_input[i]!=' ';i++){
+			instruction[i]=user_input[i];
+		}*/
+		//scanf("%s", instruction);
 	}		
 	close(fd);
 
