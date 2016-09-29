4ffcd57fc0058bdeef0641d6ecaa36942f319f41
Nuno Gonçalves <nunombg@gmail.com>
2016-09-29 16:32:19 +0100

    Code cleanup
    Some clean up including: -removal of some unnecessary ifndef and cout's
    ; -more comments and line tabs; - some missing error handlers;

--- projeto/tcs_server/tcs_server.cpp
+++ projeto/tcs_server/tcs_server.cpp
@@ -19,56 +19,37 @@
 #endif
 
 //macros for commands and errors to facilitate code reading
-#ifndef LIST_REQUEST
+#ifndef COMMANDS
 #define LIST_REQUEST "ULQ"	
-#endif
 
-#ifndef LIST_RESPONSE
 #define LIST_RESPONSE "ULR "
-#endif
 
-#ifndef CONNECT_REQUEST
-#define CONNECT_REQUEST "UNQ"
-#endif
-
-#ifndef CONNECT_RETURN
-#define CONNECT_RETURN "UNR "
-#endif
-
-#ifndef CONNECT_RETURN_UNKNOWN
-#define CONNECT_RETURN_UNKNOWN "UNR EOF\n"
-#endif
-
-#ifndef CONNECT_RETURN_ERROR
-#define CONNECT_RETURN_ERROR "UNR ERR\n"
-#endif
-
-#ifndef NEW_SERVER
-#define NEW_SERVER "SRG"
-#endif
-
-#ifndef NEW_SERVER_OK
-#define NEW_SERVER_OK "SRR OK\n"
-#endif
-
-#ifndef NEW_SERVER_NOK
-#define NEW_SERVER_NOK "SRR NOK\n"
-#endif
-
-#ifndef CLOSE_SERVER
-#define CLOSE_SERVER "SUN"
-#endif
-
-#ifndef CLOSE_SERVER_OK
-#define CLOSE_SERVER_OK "SUN OK\n"
-#endif
-
-#ifndef CLOSE_SERVER_NOK
-#define CLOSE_SERVER_NOK "SUN NOK\n"
-#endif
-
-#ifndef NO_SERVERS_ERROR
 #define NO_SERVERS_ERROR "ULR EOF\n"
+
+#define CONNECT_REQUEST "UNQ"
+
+#define CONNECT_RETURN "UNR "
+
+#define CONNECT_RETURN_UNKNOWN "UNR EOF\n"
+
+#define CONNECT_RETURN_ERROR "UNR ERR\n"
+
+#define NEW_SERVER "SRG"
+
+#define NEW_SERVER_OK "SRR OK\n"
+
+#define NEW_SERVER_NOK "SRR NOK\n"
+
+#define NEW_SERVER_ERR "SRR ERR\n"
+
+#define CLOSE_SERVER "SUN"
+
+#define CLOSE_SERVER_OK "SUN OK\n"
+
+#define CLOSE_SERVER_NOK "SUN NOK\n"
+
+#define CLOSE_SERVER_ERR "SUN ERR\n"
+
 #endif
 
 
@@ -117,62 +98,69 @@ int main(int argc, char const *argv[])
 
 	addrlen = sizeof(clientaddr); // get size of client address
 
-	while(1)
+	while(1) // main connection loop
 	{
 		char buffer[MAX_SIZE] = "";		// buffer for command input
 
 		if (recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*) &clientaddr, &addrlen) == -1) // get message from clients
-		exit(-1);
+			exit(-1);
 
 
 		sscanf(buffer, "%s",command); // determine the received command
 
-		cout << buffer;
 
 		if (!strcmp(command,LIST_REQUEST))		// list command processing
 			{
+				cout << "List request received." << endl;
 				if (servers.size() == 0) // There are no servers available
 				{
 					if(sendto(fd, NO_SERVERS_ERROR, strlen(NO_SERVERS_ERROR), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) // send no available servers error
-					exit(1);
+					exit(-1);
 				}
 				else
 				{
 					string data = LIST_RESPONSE;
 					stringstream stream;
 
+					// add current number of servers to message
 					stream << servers.size();
-					data += stream.str() + ' ';
+					data += stream.str() + ' '; 
 
-					cout << data << endl;
-
+					// add all languages names in server vector to message
 					for (unsigned i = 0; i < servers.size(); ++i)
 					{
-						cout << servers[i].language << endl;
 						data.append(servers[i].language);
 						data += ' ';
-						cout << data << endl;
 					}
 
-					data += '\n';
-
-					cout << data;
+					data += '\n'; // add new line character to end of message
 
 					if(sendto(fd, data.c_str(), strlen(data.c_str()), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) // send no available servers error
-					exit(1);
+						exit(-1);
+
+					cout << "List request sent." << endl;
 
 				}
 				
 			}
 		else if (!strcmp(command,CONNECT_REQUEST)) // translator connection request
 			{
-				char language_buffer[20];
+				char language_buffer[20] ;
 				string data = CONNECT_RETURN;
 				stringstream stream;
+
+				cout << "Connect request received." << endl;
 				
 				sscanf(buffer,"%s %s\n", command, language_buffer);
 
-				if (duplicateLanguage(servers,language_buffer))
+				if (language_buffer[0] == '\0')
+				{
+					if(sendto(fd, CONNECT_RETURN_ERROR, strlen(CONNECT_RETURN_ERROR), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) 
+						exit(-1);
+					cout << "Connect request failed. No language specified." << endl;
+				}
+
+				else if (duplicateLanguage(servers,language_buffer))
 				{
 					for (unsigned i = 0; i < servers.size(); ++i)
 					{
@@ -188,7 +176,9 @@ int main(int argc, char const *argv[])
 							data += '\n';
 
 							if(sendto(fd, data.c_str(), strlen(data.c_str()), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) // send no available servers error
-								exit(1);
+								exit(-1);
+
+							cout << "Connection data sent to client." << endl;
 
 						}
 					}
@@ -196,7 +186,7 @@ int main(int argc, char const *argv[])
 				else
 				{
 						if(sendto(fd, CONNECT_RETURN_UNKNOWN, strlen(CONNECT_RETURN_UNKNOWN), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) 
-								exit(1);
+								exit(-1);
 				}
 			}
 		
@@ -204,37 +194,49 @@ int main(int argc, char const *argv[])
 			{
 				char language_buffer[20];
 				char ip_buffer[30];
-				int port_buffer;
+				int port_buffer = -1;
 				sscanf(buffer,"%s %s %s %d\n", command, language_buffer, ip_buffer, &port_buffer); //get info from new server
 
-				if (!duplicateLanguage(servers,language_buffer))
+				if (language_buffer[0] == '\0' || ip_buffer[0] == '\0' || port_buffer == -1)
+				{
+					if(sendto(fd, NEW_SERVER_ERR, strlen(NEW_SERVER_ERR), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) //send confirmation message to translation server
+						exit(-1);
+					cout << "Failed to add TRS server. Protocol error." << endl;
+
+				}
+
+				else if (!duplicateLanguage(servers,language_buffer))
 				{
 					Server s(language_buffer,ip_buffer, port_buffer); // create new server and add to server vector
 					servers.push_back(s);
-					cout << "Added: " <<  servers[servers.size()-1].language << ' ' << servers[servers.size()-1].ip_addr << ' ' << servers[servers.size()-1].port 
-					<< ' ' << servers.size() << endl;
+					cout << "Added: " <<  servers[servers.size()-1].language << ' ' << servers[servers.size()-1].ip_addr << ' ' << servers[servers.size()-1].port << endl;
 					if(sendto(fd, NEW_SERVER_OK, strlen(NEW_SERVER_OK), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) //send confirmation message to translation server
-						exit(1); 
+						exit(-1); 
 				}
 				else
 				{
 					if(sendto(fd, NEW_SERVER_NOK, strlen(NEW_SERVER_NOK), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) //send confirmation message to translation server
-						exit(1); 
-				}
+						exit(-1);
+					cout << "Failed to add TRS server. Duplicate language." << endl;
 
-				
+				}
 			}
 		
 		else if (!strcmp(command,CLOSE_SERVER)) // remove server from available list
 			{
 				char language_buffer[20];
 				char ip_buffer[30];
-				int port_buffer;
+				int port_buffer = -1;
 				sscanf(buffer,"%s %s %s %d\n", command, language_buffer, ip_buffer, &port_buffer); // get info from server
 
+				if (language_buffer[0] == '\0' || ip_buffer[0] == '\0' || port_buffer == -1)
+				{
+					if(sendto(fd, CLOSE_SERVER_ERR, strlen(CLOSE_SERVER_ERR), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) //send confirmation message to translation server
+						exit(-1);
+					cout << "Failed to remove TRS server. Protocol error." << endl;
+				}
 
-
-				if (duplicateLanguage(servers,language_buffer))
+				else if (duplicateLanguage(servers,language_buffer))
 				{
 					for (unsigned i = 0; i < servers.size(); ++i)
 						{
@@ -243,15 +245,23 @@ int main(int argc, char const *argv[])
 									cout << "Removed: " << servers[i].language << ' ' << servers[i].ip_addr << ' ' << servers[i].port << endl;
 									servers.erase(servers.begin()+i);
 									if(sendto(fd, CLOSE_SERVER_OK, strlen(CLOSE_SERVER_OK), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) //send confirmation message to removed server
-										exit(1);
+										exit(-1);
 								}
+							else if (!strcmp(servers[i].language,language_buffer))
+							{
+								if(sendto(fd, CLOSE_SERVER_NOK, strlen(CLOSE_SERVER_NOK), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) //send confirmation message to removed server
+									exit(-1);
+								cout << "Failed to remove TRS server. IP and/or port number do not match." << endl;
+							}
 						}
 				}
 
 				else
 				{
 					if(sendto(fd, CLOSE_SERVER_NOK, strlen(CLOSE_SERVER_NOK), 0, (struct sockaddr*) &clientaddr, addrlen) == -1) //send confirmation message to removed server
-						exit(1);
+						exit(-1);
+					cout << "Failed to remove TRS server. Language does not exist." << endl;
+
 				}
 			}
 	}
